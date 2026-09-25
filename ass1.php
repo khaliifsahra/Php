8777415c354cf0bdb3c7896aa68b90fd091e44f9
Zahra khaliif <khaliifsahra84@gmail.com>
2026-09-25 08:48:34 +0300

    Add files via upload

---
+++ ass1.php
@@ -0,0 +1,14 @@
+<?php
+$n1 = 10;
+$n2 = 20;
+$n3 = 30;
+// ineed greather num
+if ($n1 >= $n2 && $n3) {
+    echo ("this nums is  same ");
+
+} else {
+    echo ("this nums is not same ");
+}
+
+
+?>
