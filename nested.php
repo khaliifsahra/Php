f48647a4b72a7ab2d452ecec0cc1921ea71807af
Zahra khaliif <khaliifsahra84@gmail.com>
2026-09-25 08:13:13 +0300

    Add files via upload

---
+++ nested.php
@@ -0,0 +1,6 @@
+<?php
+for ($i = 1; $i <= 3; $i++)
+    // echo ("row is 1");
+    for ($j = 1; $j <= 5; $j++)
+        echo ("row is 1," . "$i* $j=" . "colum is 1 ," . "result is 1 ," . ($i * $j) . "<br>");
+?>
